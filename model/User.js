07f9f39030e85609be82e142bac6634db01f3c98
Aryan Vishwakarma <aryan.vishwakarma2020@vitstudent.ac.in>
2022-10-19 20:52:35 +0530

    User Model

---
+++ model/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
